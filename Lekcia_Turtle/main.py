320229d0ab7e815a57f88da01932079f37ec12d3
MPalso <martin.palsovic@live.com>
2024-04-27 22:51:36 +0200

    Vyplnenie tvaru (fill)

--- Lekcia_Turtle/main.py
+++ Lekcia_Turtle/main.py
@@ -2,15 +2,15 @@ from turtle import Turtle, Screen
 import random
 import turtle
 
-#zmena farevneho modu
-turtle.colormode(255)
+
 
 tommy = Turtle()
-tommy.shape("turtle")
-tommy.color("green")
-tommy.pensize(2)
-
+tommy.shape("arrow")
+tommy.color("red")
+tommy.pensize(1)
+###############################
 #######ukony tommyho krok po korku
+###################################
 """ tommy.forward(100)
 tommy.right(90)
 tommy.forward(100)
@@ -24,8 +24,11 @@ tommy.right(90) """
 """ for _ in range(4):
     tommy.right(90)
     tommy.forward(100) """
-    
+
+
+#######################################   
 #######tommyho prerusovana ciara po kroku
+###########################################
 """ tommy.pendown()
 tommy.forward(20)
 tommy.penup()
@@ -49,7 +52,9 @@ tommy.forward(20) """
     tommy.penup()
     tommy.forward(20)
  """
+##############################################
 #####farebna kresba s nahodnymi farbami a vzdy o uhol viac
+##############################################
 """ farby = ["green", "red", "blue", "yellow", "black", "pink"] """
 
 #krok po korku
@@ -76,12 +81,17 @@ while pohyb != 9:
         tommy.right(360/pohyb)
     pohyb += 1 """
 
-##### Random walk / nahodny pohyb tommyho
 
+#########################################
+##### Random walk / nahodny pohyb tommyho
+#########################################
 #list farieb_toto som zrusil, lebo som pouzil tuple radnom color()
 #farby = ["green", "red", "blue", "yellow", "black", "pink"]
 
-def random_color():
+#zmena farevneho modu
+"""turtle.colormode(255)"""
+
+""" def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
@@ -108,7 +118,58 @@ for number in range (200):
     #Zvysenie rychlosti
     tommy.speed(speed)
     speed += 1
+ """
 
+
+##########################################
+#####SPIROMETER
+##########################################
+
+#Zmena farebneho modu
+""" turtle.colormode(255)
+tommy.speed(20)
+
+#Funkcia na generovanie farby
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+def  spirograf(gap):
+    for number in range(int(360/gap)):
+        tommy.pencolor(random_color())
+        tommy.circle(80)
+        tommy.left(gap)
+
+spirograf(5) """
+
+########################################
+######DIAGRAM
+########################################
+
+""" colors = ["red","green","blue","yellow","pink","purple"]
+
+arrow = Turtle("arrow")
+
+for x in range(200):
+    arrow.pencolor(colors[x%6])
+    arrow.forward(x)
+    arrow.left(60) """
+
+
+################################
+#####VYPLNENIE TVARU (FILL)
+################################
+""" tommy.pen(fillcolor="red")
+tommy.begin_fill()
+
+for _ in range(4):
+    tommy.forward(80)
+    tommy.left(90)
+
+tommy.end_fill() """
 
 
 my_screen = Screen()
